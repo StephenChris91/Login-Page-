cda6d3960583a2aed9efa418c168a973ac9e4014
StephenChris91 <46078420+StephenChris91@users.noreply.github.com>
2019-08-22 21:03:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+li = document.createElement('li');
+li.className = 'classes';
+li.setAttribute('title', 'stephen')
+
+li.appendChild(document.createTextNode('Stephen Created A Node'))
+
+console.log(li)
